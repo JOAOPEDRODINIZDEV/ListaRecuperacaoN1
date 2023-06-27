3d129efc5f1e2cdad3702fe467e8201de1b6db57
JOAOPEDRODINIZDEV <115243882+JOAOPEDRODINIZDEV@users.noreply.github.com>
2023-06-27 18:31:40 -0300

    Add files via upload

--- ListaRecperação/jogo.py
+++ ListaRecperação/jogo.py
@@ -2,9 +2,12 @@ import random
 palpite = int(input("Qual é o seu númeo do palpite entre 1 e 50:  "))
 numero_sorteado = random.randint(1,50)
 
-if palpite > numero_sorteado:
-    print("Muito Alto")
-elif palpite < numero_sorteado:
-    print("Muito Baixo")
-else:
-    print("Você Acertou")
+while palpite != numero_sorteado:
+    if palpite > numero_sorteado:
+        print("Muito Alto")
+        palpite = int(input("Qual é o seu númeo do palpite entre 1 e 50:  "))        
+    elif palpite < numero_sorteado:
+        print("Muito Baixo")
+        palpite = int(input("Qual é o seu númeo do palpite entre 1 e 50:  "))
+        
+print("Você Acertou")
